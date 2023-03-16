8001aaa7766387679c6ad54cf78d6d6d68c514ea
Min Thu Zaw <minthuzaw775@gmail.com>
2023-03-16 23:09:05 +0630

    added: lazy load image test

--- routes/web.php
+++ routes/web.php
@@ -7,6 +7,9 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+Route::get('/lazy-load', function () {
+    return view('lazy-load-image');
+});
 
 
 Route::get('/dashboard', function () {
